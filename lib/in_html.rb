c8356e3333149456d8f5b77b1dfff13bd611cb86
ayarem21 <ayarem21@gmail.com>
2019-10-29 16:18:07 +0200

    Valid_HTML

--- lib/in_html.rb
+++ lib/in_html.rb
@@ -7,9 +7,13 @@ class In_HTML
       content_ = content
     end
     f = File.new("index.html", "w")
+    f.puts("<!DOCTYPE html>\n")
+    f.puts("<html lang='en'>\n")
+    f.puts("<head>\n<meta charset='utf-8'>\n<title>Title</title>\n</head>\n")
     f.puts("<body>\n")
-    f.write("<div>#{content_}</div>\n")
-    f.write("<body>")
+    f.puts("<div>#{content_}</div>\n")
+    f.puts("<body>\n")
+    f.puts("</html>\n")
     f.close
   end
 end
